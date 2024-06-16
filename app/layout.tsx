9b76a32bc283eae7bb894582323903f2f4d7a09a
tingfeng <sunny-wind@foxmail.com>
2024-06-16 09:50:01 +0800

    feat: logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +31,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} p-12`}>
         <Providers>{children}</Providers>
+        <div className="text-3xl font-semibold opacity-20 fixed bottom-4 flex items-center">
+          Trip AI
+          <a href="https://github.com/5unnyWind/trip-ai">
+            <GitHubLogoIcon className=" w-8 h-8 ml-4" />
+          </a>
+        </div>
         <Toaster />
       </body>
     </html>
